6a4211cb90b3d7d3aa05c5685281b8d00e44491f
schatten <dipanjan.c@udaan.com>
2020-01-29 19:47:51 +0530

    Finished chapter 4

--- src/Game.kt
+++ src/Game.kt
@@ -4,13 +4,35 @@ fun main(args: Array<String>) {
     val isBlessed = true
     val isImmortal = false
 
-    // Compute Aura
-    val auraVisible = isBlessed && playerHealth > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, playerHealth, isImmortal)
+    val healthStatus = formatHealthStatus(playerHealth, isBlessed)
+    val inebriationStatus = castFireball(2)
 
-    println("Aura color: $auraColor, isBlessed: ${ if (isBlessed) "Yes" else "No" }")
+    printHealthStatus(auraColor, isBlessed, playerName, healthStatus)
+    printInebriationStatus(inebriationStatus)
+}
 
-    val healthStatus = when (playerHealth) {
+fun printInebriationStatus(inebriationStatus: Int): Unit {
+    val status = when(inebriationStatus) {
+        in 1..10 -> "tipsy"
+        in 11..20 -> "sloshed"
+        in 21..30 -> "soused"
+        in 31..40 -> "stewed"
+        else -> "..t0aSt3d"
+    }
+
+    print("Inebriation Status: $status")
+}
+
+private fun printHealthStatus(auraColor: String, isBlessed: Boolean, playerName: String, healthStatus: String) {
+    println("Aura color: $auraColor, isBlessed: ${if (isBlessed) "Yes" else "No"}")
+    println("$playerName $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, playerHealth: Int, isImmortal: Boolean) = if (isBlessed && playerHealth > 50 || isImmortal) "GREEN" else "NONE"
+
+private fun formatHealthStatus(playerHealth: Int, isBlessed: Boolean): String {
+    return when (playerHealth) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches"
         in 75..89 -> if (isBlessed) {
@@ -21,7 +43,9 @@ fun main(args: Array<String>) {
         in 15..74 -> "looks pretty hurt"
         else -> "is in awful condition"
     }
+}
 
-
-    println("$playerName $healthStatus")
+private fun castFireball(numTimes: Int = 2): Int {
+    println("A fireball was cast into existence. (x$numTimes)")
+    return minOf(numTimes * 10, 50)
 }
